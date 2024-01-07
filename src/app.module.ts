18b7e31fb884f055a1e3f748eac845bc3d61ee5c
lesythanh <lesythanh063@gmail.com>
2024-01-07 15:06:05 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb+srv://lesythanh063:lesythanh063@cluster0.ww0jima.mongodb.net/`)],
   controllers: [AppController],
   providers: [AppService],
 })
