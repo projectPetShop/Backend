7dd3742bb6832c5551844462c410ae8615b066e6
anhtuyetdut <anhtuyetdutk19@gmail.com>
2024-01-17 22:43:42 +0700

    fix: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
+  app.enableCors();
   await app.listen(PORT);
 
   const logger = new Logger('Pet Shop');
